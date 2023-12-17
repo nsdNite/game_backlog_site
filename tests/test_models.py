63fbfd0531246c534ae489709deaf9c7fe351f3d
nsdNite <nsd7night@gmail.com>
2023-12-17 11:45:44 +0200

    Update ToggleGameBacklog to Class view, flake8 tests
    Now all views are Classes, update flake8 tests, update urls with class views.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from backlog.models import Game, Gamer, Developer
+from backlog.models import Game, Developer
 
 
 class ModelTests(TestCase):
