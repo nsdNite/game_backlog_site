0b419f3aa1e8712c4e9f6f4c5dc57e3e851f78de
nsdNite <nsd7night@gmail.com>
2023-12-20 13:22:43 +0200

    Update settings with Render deployment recommendations

--- game_backlog_site/settings.py
+++ game_backlog_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
